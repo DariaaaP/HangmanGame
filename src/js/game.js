a6ac50c06a5d717af452972323746fa5abc7aea8
DariaaaP <barinova_f@list.ru>
2023-08-19 18:02:04 +0400

    added game's logic & some style for buttons

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,8 @@ import { WORDS, KEYBOARD_LETTERS } from "./consts";
 const gameDiv = document.querySelector("#game"),
   logoH1 = document.querySelector("#logo");
 
+let triesLeft;
+
 const createPlaceholderHTML = () => {
   const word = sessionStorage.getItem("word");
   const wordArray = word.split("");
@@ -39,18 +41,47 @@ const createHangmanImg = () => {
   return image;
 };
 
+const checkLetter = (letter) => {
+  const word = sessionStorage.getItem("word");
+  const inputLetter = letter.toLowerCase();
+  //буквы в слове нет
+  if (!word.includes(inputLetter)) {
+    const triesCounter = document.querySelector("#tries-left");
+    triesLeft -= 1;
+    triesCounter.innerText = triesLeft;
+
+    const hangmanImg = document.querySelector("#hangman-img");
+    hangmanImg.src = `images/hg-${10 - triesLeft}.png`;
+  } else {
+    //буква в слове есть
+    const wordArray = Array.from(word);
+    wordArray.forEach((currentLetter, i) => {
+      if (currentLetter === inputLetter) {
+        document.querySelector(`#letter_${i}`).innerText =
+          inputLetter.toUpperCase();
+      }
+    });
+  }
+};
+
 export const startGAme = () => {
+  triesLeft = 10;
   logoH1.classList.add("logo-sm");
   const randomIndex = Math.floor(Math.random() * WORDS.length);
   const wordToGuess = WORDS[randomIndex];
   sessionStorage.setItem("word", wordToGuess);
 
   gameDiv.innerHTML = createPlaceholderHTML();
-  gameDiv.innerHTML +=
-    "<p id='tries' class='mt-2'>TRIES LEFT: <span id='tries-left' class='font-medium text-green-600'>10</span></p>";
+  gameDiv.innerHTML += `<p id='tries' class='mt-2'>TRIES LEFT: <span id='tries-left' class='font-medium text-green-600'>${triesLeft}</span></p>`;
 
-  // const keyboardDiv = createKeyboard();
-  // keyboardDiv.addEventListener("click", (event) => {});
+  const keyboardDiv = createKeyboard();
+
+  keyboardDiv.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+      event.target.disabled = true;
+      checkLetter(event.target.id);
+    }
+  });
 
   const hangmanImg = createHangmanImg();
   gameDiv.prepend(hangmanImg);
