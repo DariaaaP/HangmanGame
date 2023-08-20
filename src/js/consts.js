2b3bd3667fc3e5cb373cdac3e73aeeac9eb12ffa
DariaaaP <barinova_f@list.ru>
2023-08-20 16:27:17 +0400

    added more words

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,13 @@
-export const WORDS = ["code", "frontend", "programming"];
+export const WORDS = [
+  "code",
+  "frontend",
+  "programming",
+  "backend",
+  "algorithms",
+  "asynchronous",
+  "github",
+  "object",
+  "function",
+];
 
 export const KEYBOARD_LETTERS = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
